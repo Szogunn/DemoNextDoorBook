5df4ecf355d234e006d0fc32c2bfc04b9c0b5b2c
szogunn <mateusz1_boczek1@interia.pl>
2024-01-19 21:40:33 +0100

    Added Service annotation to UserService

--- src/main/java/com/szogunn/demonextdoorbook/servicesImpl/UserServiceImpl.java
+++ src/main/java/com/szogunn/demonextdoorbook/servicesImpl/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package com.szogunn.demonextdoorbook.servicesImpl;
 
 import com.szogunn.demonextdoorbook.services.UserService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements UserService {
 }

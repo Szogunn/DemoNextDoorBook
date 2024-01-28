414facb0be70a640088fb3091d41578d7b562cc7
szogunn <mateusz1_boczek1@interia.pl>
2024-01-28 19:20:15 +0100

    Added Builder to BookDTO

--- src/main/java/com/szogunn/demonextdoorbook/dtos/BookDTO.java
+++ src/main/java/com/szogunn/demonextdoorbook/dtos/BookDTO.java
@@ -1,17 +1,77 @@
 package com.szogunn.demonextdoorbook.dtos;
 
 import com.szogunn.demonextdoorbook.model.Author;
+import com.szogunn.demonextdoorbook.model.Book;
+import com.szogunn.demonextdoorbook.model.User;
 
 import java.time.LocalDate;
 import java.util.Set;
 
 public record BookDTO(
-        String tittle,
+        Long id,
+        String title,
         String ISBN,
         int numPages,
         String language,
         String publisher,
         LocalDate publishedYear,
-        Set<Author> authors
+        Set<Author> authors,
+        String owner
 ) {
+    public static class Builder {
+        private Long id;
+        private String title;
+        private String ISBN;
+        private int numPages;
+        private String language;
+        private String publisher;
+        private LocalDate publishedYear;
+        private Set<Author> authors;
+        private String owner;
+
+        public Builder(){
+        }
+
+        public Builder id(Long id){
+            this.id = id;
+            return this;
+        }
+        public Builder title(String title){
+            this.title = title;
+            return this;
+        }
+        public Builder ISBN(String ISBN){
+            this.ISBN = ISBN;
+            return this;
+        }
+        public Builder numPages(int numPages){
+            this.numPages = numPages;
+            return this;
+        }
+        public Builder language(String language){
+            this.language = language;
+            return this;
+        }
+        public Builder publisher(String publisher){
+            this.publisher = publisher;
+            return this;
+        }
+        public Builder publishedYear(LocalDate publishedYear){
+            this.publishedYear = publishedYear;
+            return this;
+        }
+        public Builder authors(Set<Author> authors){
+            this.authors = authors;
+            return this;
+        }
+        public Builder owner(String owner){
+            this.owner = owner;
+            return this;
+        }
+
+        public BookDTO build() {
+            return new BookDTO(id, title, ISBN, numPages, language, publisher, publishedYear, authors, owner);
+        }
+
+    }
 }
